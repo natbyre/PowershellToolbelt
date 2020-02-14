321f5c88fcee475cb7d828d77924a5c5f77bc0cf
neilcccc <neilcccc@gmail.com>
2020-02-14 17:50:17 +0000

    Update when-last-reboot.ps1
    replaces the default LastBootupTime format (yyyyMMddhhmmss)
    with more readable:  yyyy-MM-dd hh:mm

--- servers/when-last-reboot.ps1
+++ servers/when-last-reboot.ps1
@@ -4,4 +4,4 @@ param(
     [Parameter(Mandatory = $True,valueFromPipeline=$true)][String] $serverName
     )
 
-Get-WmiObject -Class Win32_OperatingSystem -ComputerName $serverName | Select-Object CSName, LastBootupTime
+Get-WmiObject -Class Win32_OperatingSystem -ComputerName $serverName | Select-Object CSName, @{Name='LastBootupTime'; Expression={"$($($_.LastBootupTime).SubString(0,4))-$($($_.LastBootupTime).SubString(4,2))-$($($_.LastBootupTime).SubString(6,2)) $($($_.LastBootupTime).SubString(8,2)):$($($_.LastBootupTime).SubString(10,2))"}}
